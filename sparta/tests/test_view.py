79d5d7fdff07b39e4cf241d33d1c21387fd0c60a
yongheon-Lee <dydgjs778@gmail.com>
2022-02-16 22:31:11 +0900

    yml file add

--- sparta/tests/test_view.py
+++ sparta/tests/test_view.py
@@ -3,8 +3,7 @@ from django.test import TestCase
 
 
 class TestView(TestCase):
-
     def test_add_view(self) -> None:
-        result = self.client.get('/api/add', {'a': 46, 'b': 4861}) # urls에 만들었던 add 호출
-        self.assertEqual(result.status_code, 200)                  # 응답값 제대로 오는지 확인
-        self.assertEqual(result.json(), {'result': 4907})          # 결과 검증poetry add isort==5.10.1
+        result = self.client.get("/api/add", {"a": 46, "b": 4861})  # urls에 만들었던 add 호출
+        self.assertEqual(result.status_code, 200)  # 응답값 제대로 오는지 확인
+        self.assertEqual(result.json(), {"result": 4907})  # 결과 검증poetry add isort==5.10.1
