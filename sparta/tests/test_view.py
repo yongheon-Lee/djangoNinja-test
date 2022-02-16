80376c74c01c6cd39a7f508d7fd2c4f34109256e
yongheon-Lee <dydgjs778@gmail.com>
2022-02-16 22:13:58 +0900

    second commit

---
+++ sparta/tests/test_view.py
@@ -0,0 +1,10 @@
+# 테스트 케이스 만들기
+from django.test import TestCase
+
+
+class TestView(TestCase):
+
+    def test_add_view(self) -> None:
+        result = self.client.get('/api/add', {'a': 46, 'b': 4861}) # urls에 만들었던 add 호출
+        self.assertEqual(result.status_code, 200)                  # 응답값 제대로 오는지 확인
+        self.assertEqual(result.json(), {'result': 4907})          # 결과 검증
--- sparta/urls.py
+++ sparta/urls.py
@@ -13,19 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from typing import Dict
+
 from django.contrib import admin
+from django.http import HttpRequest
 from django.urls import path
 from ninja import NinjaAPI
 
 api = NinjaAPI()
 
 
-@api.get('/add')
-def add(request, a:int, b:int):
-    return {'result': a+b}
+@api.get("/add")
+def add(request: HttpRequest, a: int, b: int) -> Dict[str, int]:
+    return {"result": a + b}
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', api.urls),  # api 경로에 urls 등록
+    path("admin/", admin.site.urls),
+    path("api/", api.urls),  # api 경로에 urls 등록
 ]
